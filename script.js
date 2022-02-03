da8629f38f432a74f46b01c9c8e88fe174ef9d53
chile omereji <chileomereji@gmail.com>
2022-02-03 13:48:46 +0100

    initial files

---
+++ script.js
@@ -0,0 +1,142 @@
+function displayCity(){
+let v;
+let val1;
+let val2 = document.getElementById("input_field1").value;
+let val3 = document.getElementById("input_field2");
+let val4 = val2.toLowerCase();
+switch (val4){
+case "aba":
+
+    val1 = 440001;
+    
+    break;
+    case "adamawa":
+        val1 = 640001;
+        break;
+        case "akwa ibom":
+            val1 = "520001";
+            break;
+            case "Anambra":
+                val1 = "420001";
+                break;
+                case "bauchi":
+                    val1 = "740001";
+                    break;
+                    case "bayelsa":
+                        val1 = "561001";
+                        break;
+                        case "benue":
+                            val1 = "970001";
+                            break;;
+                            case "borno":
+                                val1 = "60001";
+                                break;
+                                case "cross rivers":
+                                    val1 ="540001";
+                                    break;
+                                    case "portharcourt":
+                                        val1 = "500001";
+                                        break;
+                                        case "delta":
+                                            val1 = "320001";
+                                            break;
+                                            case "ebony":
+                                                val1 = "840001";
+                                                break;
+                                                case "edo":
+                                                    val1 ="30001";
+                                                    break;
+                                                    case "ekiti":
+                                                        val1 ="360001";
+                                                        break;
+                                                        case "enugu":
+                                                            val1 = "400001";
+                                                            break;
+                                                            case "gombe":
+                                                                val1 = "760001";
+                                                                break;
+                                                                case "imo":
+                                                                    val1 = "460001";
+                                                                    break;
+                                                                    case "jigawa":
+                                                                        val1 = "720001";
+                                                                        break;
+                                                                        case "kaduna":
+                                                                            val1 = "700001";
+                                                                            break;
+                                                                            case "kano":
+                                                                                val1 = "800001";
+                                                                                break;
+                                                                                case "katsina":
+                                                                                    val1 = "820001";
+                                                                                    break;
+                                                                                    case "kebbi":
+                                                                                        val1 ="860001";
+                                                                                        break;
+                                                                                        case "kogi":
+                                                                                            val1 = "260001";
+                                                                                            break;
+                                                                                            case "kwara":
+                                                                                                val1 = "240001";
+                                                                                                break;
+                                                                                                case "lagos":
+                                                                                                    val1 = "100001";
+                                                                                                    break;
+                                                                                                    case "nasarawa":
+                                                                                                        val1 ="962001";
+                                                                                                        break;
+                                                                                                        case "niger":
+                                                                                                            val1 = "920001";
+                                                                                                            break;
+                                                                                                            case "ogun":
+                                                                                                                val1 = "110001";
+                                                                                                                break;
+                                                                                                                case "ondo":
+                                                                                                                    val1 = "340001";
+                                                                                                                    break;
+                                                                                                                    case "osun":
+                                                                                                                        val1 ="230001";
+                                                                                                                        break;
+                                                                                                                        case "oyo":
+                                                                                                                            val1 = "200001";
+                                                                                                                            break;
+                                                                                                                            case "plateau":
+                                                                                                                                val1 = "930001";
+                                                                                                                                break;
+                                                                                                                                case "sokoto":
+                                                                                                                                    val1 = "840001";
+                                                                                                                                    break;
+                                                                                                                                    case "taraba":
+                                                                                                                                        val1 = "660001";
+                                                                                                                                        break;
+                                                                                                                                        case "yobe":
+                                                                                                                                            val1 = "320001";
+                                                                                                                                            break;
+                                                                                                                                            case "zamfara":
+                                                                                                                                                val1 = "860001";
+                                                                                                                                                break;
+                                                                                                                                                case "abuja":
+                                                                                                                                                    val1 = "900001";
+                                                                                                                                                    break;
+                                                            
+                                        default:
+                                            val1 = "Enter a valid city in Nigeria";
+
+            
+        
+
+
+
+
+    
+    
+
+
+}
+val3.value = val1;
+
+
+
+}
+
+
